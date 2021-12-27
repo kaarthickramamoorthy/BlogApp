d59c5a5f522c3d4177673668966210fa37c03ec4
Kaarthick Rama Moorthy <kaarthick.r.1982@gmail.com>
2021-12-27 13:03:42 -0500

    Custom Validation Error response - Extend ResponseEntityExceptionHandler and override handleMethodArgumentNotValid()

--- src/main/java/com/prathickya/blogApp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prathickya/blogApp/exception/GlobalExceptionHandler.java
@@ -3,17 +3,22 @@ package com.prathickya.blogApp.exception;
 import com.prathickya.blogApp.payload.BlogAppCustomErrorResponse;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<BlogAppCustomErrorResponse> handleResourceNotFoundException(
@@ -43,5 +48,20 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now(), exception.getMessage(),
                 webRequest.getDescription(Boolean.FALSE));
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            final String fieldName = ((FieldError) error).getField();
+            final String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
